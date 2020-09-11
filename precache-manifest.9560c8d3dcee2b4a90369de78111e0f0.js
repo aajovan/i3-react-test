7c5b070b14e91ecd03b467c98c6e4e54fc8488e8
aajovan <alenjovancom@gmail.com>
2020-09-11 12:49:17 +0200

    Updates

---
+++ precache-manifest.9560c8d3dcee2b4a90369de78111e0f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28d7216d05996dcd2e9d54280648e8f",
+    "url": "/i3_test/index.html"
+  },
+  {
+    "revision": "789beeea22b19c48b819",
+    "url": "/i3_test/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6c64bb489b6378daa5b1",
+    "url": "/i3_test/static/css/main.ea9d7ad0.chunk.css"
+  },
+  {
+    "revision": "789beeea22b19c48b819",
+    "url": "/i3_test/static/js/2.38b56a1e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/i3_test/static/js/2.38b56a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c64bb489b6378daa5b1",
+    "url": "/i3_test/static/js/main.ddb83464.chunk.js"
+  },
+  {
+    "revision": "c7ed3619ea174b935867",
+    "url": "/i3_test/static/js/runtime-main.b9544533.js"
+  }
+]);
